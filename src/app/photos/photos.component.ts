88d80b5538a7f478443e40c5c9942fbb8d338ac3
DongDuong9E <dongduongdev@gmail.com>
2020-12-04 20:29:39 +0700

    Refractor

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
+
+
 export class PhotosComponent implements OnInit {
 
   constructor() { }
@@ -12,52 +15,7 @@ export class PhotosComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  pts1 = [
-    "pts1_1.jpg",
-    "pts1_2.jpg",
-    "pts1_3.jpg",
-    "pts1_4.jpg",
-    "pts1_5.jpg",
-    "pts1_6.jpg",
-    "pts1_7.jpg",
-    "pts1_8.jpg",
-    "pts1_9.jpg",
-    "pts1_10.jpg",
-  ];
-
-  pts2 = [
-    "pts2_1.jpg",
-    "pts2_2.jpg",
-    "pts2_3.jpg",
-    "pts2_4.jpg",
-    "pts2_5.jpg",
-    "pts2_6.jpg",
-    "pts2_7.jpg",
-    "pts2_8.jpg",
-    "pts2_9.jpg",
-    "pts2_10.jpg",
-    "pts2_11.jpg",
-    "pts2_12.jpg",
-    "pts2_13.jpg",
-    "pts2_14.jpg",
-    "pts2_15.jpg",
-    "pts2_16.jpg",
-    "pts2_17.jpg",
-    "pts2_18.jpg",
-    "pts2_19.jpg",
-    "pts2_20.jpg",
-    "pts2_21.jpg",
-    "pts2_22.jpg",
-    "pts2_23.jpg",
-    "pts2_24.jpg",
-    "pts2_25.jpg",
-    "pts2_26.jpg",
-    "pts2_27.jpg",
-    "pts2_28.jpg",
-    "pts2_29.jpg",
-    "pts2_30.jpg",
-    "pts2_31.jpg",
-    "pts2_32.jpg"
-  ];
+  pts1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  pts2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32];
 
 }
